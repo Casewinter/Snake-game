3eccf26c072173aaf1c126465b1b61bdf33a55fd
Casewinter <viniciusbassilio@gmail.com>
2023-12-12 11:18:00 -0300

    Preciso concertar o array da cobra

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Snake-game-IA
 
-[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Casewinter/Snake-game-IA)
--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 import './style.css';
+import {collideRect} from './utils/ultils'
 
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
+const canvasWidth = 600
+const canvasHeight = 600
+const squareSize = 40
+ 
+
 class Snake {
-  constructor(ctx) {
+  constructor(ctx, size) {
     this.ctx = ctx;
     //tamanho em pixels de cada lado da cobra
-    this.size = 30;
+    this.size = size
     //cada array contém as coordenadas em pixel
     //de onde esta aquele quadrado da calda.
     this.snake = [
       {
-        x: 230,
+        x: 80,
         y: 200,
       },
       {
-        x: 260,
+        x: 120,
+        y: 200,
+      },
+      {
+        x: 160,
+        y: 200,
+      },
+      {
+        x: 200,
+        y: 200,
+      },
+      {
+        x: 240,
+        y: 200,
+      },
+      {
+        x: 280,
         y: 200,
       },
     ];
@@ -25,8 +47,10 @@ class Snake {
     this.lastOrientation = 'x';
     this.orientation;
   }
+
   render() {
     this.ctx.fillStyle = '#787878';
+
     this.snake.forEach((square, index) => {
       if (index == this.snake.length - 1) {
         this.ctx.fillStyle = 'white';
@@ -34,7 +58,9 @@ class Snake {
 
       this.ctx.fillRect(square.x, square.y, this.size, this.size);
     });
+
   }
+
   move() {
 
     //pegando o ultimo elemento do array
@@ -73,20 +99,55 @@ class Snake {
         break;
     }
   }
-  update() {
-    this.move();
-    this.render();
+  collision() {
+    //Colisao contra as bordas
+    const x = this.snake.at(-1).x + this.size / 2 < 0 || this.snake.at(-1).x + this.size / 2 > 600
+    const y = this.snake.at(-1).y + this.size / 2 < 0 || this.snake.at(-1).y + this.size / 2 > 600
+
+
+   if(x || y) return true
+
+    //Colisao contra si mesmo
+  for(let i = 0; i < this.snake.length - 1; i++){
+    const head = this.snake.at(-1);
+    //const square = this.snake[i].x, this.snake[i].y, this.size, this.size
+    if(collideRect(this.snake[i], head)) return true
   }
+    return false
+   
+  }
+
+  update() {
+    if (!this.collision()) {
+      this.move();
+    } 
+    this.render()
+  }
+
+
 }
 
-const s = new Snake(ctx);
+const s = new Snake(ctx, squareSize);
 
 setInterval(() => {
   ctx.clearRect(0, 0, 600, 600);
+  for (let x = 0; x < canvasWidth; x += squareSize) {
+    for (let y = 0; y < canvasHeight; y += squareSize) {
+      ctx.fillStyle = '#242424'; // Define a nova cor aqui
+      ctx.fillRect(x, y, squareSize, squareSize);
+      ctx.strokeRect(x, y, squareSize, squareSize);
+    }
+  }
   s.update();
 }, 300);
 
 
+
+
+
+
+
+//Renderiza e aplica os controles
 let lastKeyAxies = 'x'
 
 window.addEventListener('keydown', (event) => {
